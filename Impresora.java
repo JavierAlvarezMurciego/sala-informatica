8c364b283fa8dddc436f4e1612d0495ffc9daace
Javier <alvmurjav@gmail.com>
2021-11-24 09:55:39 +0100

    01 Estructura y atributos del constructor

---
+++ Impresora.java
@@ -0,0 +1,10 @@
+public class Impresora{
+    private boolean aColor;
+    private String marca;
+
+    public Impresora(String modelo, boolean color){
+        marca = modelo;
+        aColor = color;
+    }
+
+}
